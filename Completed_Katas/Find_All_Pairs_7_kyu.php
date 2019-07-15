9697d77b4bec1ffe78aba23075b4fadd59b0e827
Cole Williams <wcole2222@gmail.com>
2019-07-15 12:36:01 -0600

    solution for find all pairs.php

---
+++ Completed_Katas/Find_All_Pairs_7_kyu.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+www.codewars.com
+
+You are given array of integers, your task will be to count all pairs in that array and return their count.
+
+Notes:
+
+Array can be empty or contain only one value; in this case return 0
+If there are more pairs of a certain number, count each pair only once. E.g.: for [0, 0, 0, 0] the return value is 2 (= 2 pairs of 0s)
+Random tests: maximum array length is 1000, range of values in array is between 0 and 1000
+Examples
+[1, 2, 5, 6, 5, 2]  -->  2
+...because there are 2 pairs: 2 and 5
+
+[1, 2, 2, 20, 6, 20, 2, 6, 2]  -->  4
+...because there are 4 pairs: 2, 20, 6 and 2 (again)
+
+
+
+*/
+function duplicates($array){
+ 
+  echo "Input: ";
+  print_r($array);
+  $compare = array();
+  $final = 0;
+
+  foreach($array as $el){
+    if(array_search($el, $array) > -1){
+      $compare[$el] += 1;
+    } else {
+       $compare[$el] = 1;
+    }
+  }
+  
+  foreach($compare as $el){
+    echo "This is the last loop: $el \n";
+    if($el > 1){
+      $final += floor($el /2);
+    }
+  }
+  
+  print_r($compare);
+  echo "TARGET: ". $final . "NEXT RUN --> \n";
+  return $final;
+}
